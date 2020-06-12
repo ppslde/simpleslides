4eca457566c06e10723b52863eaab0bd8404f265
Danny <danny.schneider.professional@gmail.com>
2020-06-12 22:44:22 +0200

    Initial commit .NET only

--- cli/Program.cs
+++ cli/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace cli {
   class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
     }
   }
 }
--- slides.core/Class1.cs
+++ slides.core/Class1.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace slides.core {
-  public class Class1 {
-  }
+namespace slides.core
+{
+    public class Class1
+    {
+    }
 }
